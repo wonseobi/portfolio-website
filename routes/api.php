d6ad3095469a2ae730584443493e9d674a368845
Gorka Treviño <108596802+gorkatrevino@users.noreply.github.com>
2025-05-22 16:19:14 -0600

    feat: Implementar middleware WHMCS para verificar disponibilidad del servicio

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\whmcs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,9 @@ Route::get('/user', function (Request $request) {
     $client = \Whmcs::Client();
 
 
-    // dd($client->GetClients());
-    dd($client->GetClientPassword(['email' => 'desarrollo@certerus.com']));
+    dd($client->GetClients());
+    // dd($client->GetClientPassword(['email' => 'desarrollo@certerus.com']));
     // dd($client->GetClientPassword(['email' => 'omar@nubew.com']));
 
     return $request->user();
-});
+})->middleware(whmcs::class);
