afbadce0d830e085ccd7bf586a66184cc6d9e814
Gorka Treviño <108596802+gorkatrevino@users.noreply.github.com>
2025-05-22 16:25:55 -0600

    feat: Reorganizar la ruta de usuario para aplicar middleware WHMCS

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,16 @@ use App\Http\Middleware\whmcs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    $client = \Whmcs::Client();
+
+Route::middleware(whmcs::class)->group(function () {
+    Route::get('/user', function (Request $request) {
+        $client = \Whmcs::Client();
 
 
-    dd($client->GetClients());
-    // dd($client->GetClientPassword(['email' => 'desarrollo@certerus.com']));
-    // dd($client->GetClientPassword(['email' => 'omar@nubew.com']));
+        dd($client->GetClients());
+        // dd($client->GetClientPassword(['email' => 'desarrollo@certerus.com']));
+        // dd($client->GetClientPassword(['email' => 'omar@nubew.com']));
 
-    return $request->user();
-})->middleware(whmcs::class);
+        return $request->user();
+    });
+});
